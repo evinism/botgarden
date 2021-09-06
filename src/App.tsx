9ed7b73d528a89524af73e7c50942c4486ca3866
Evin Sellin <evinism@gmail.com>
2021-09-06 12:38:25 -0700

    allow people to choose bots

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,39 @@
+import { useState } from "react";
 import "./App.css";
 import Game from "./Game";
+import { BotConfig, defaultBots } from "./bot";
 
 function App() {
+  const [currentBot, setCurrentBot] = useState<BotConfig | void>();
+
   return (
     <div className="App">
       <header className="App-header">
-        <Game />
+        {currentBot ? (
+          <>
+            <button onClick={() => setCurrentBot()}>Back</button>
+            <Game
+              participants={{
+                w: {
+                  type: "bot",
+                  config: currentBot,
+                },
+                b: {
+                  type: "interactive",
+                },
+              }}
+            />
+          </>
+        ) : (
+          <>
+            <h1>Choose a bot</h1>
+            {defaultBots.map((bot) => (
+              <div>
+                <button onClick={() => setCurrentBot(bot)}>{bot.name}</button>
+              </div>
+            ))}
+          </>
+        )}
       </header>
     </div>
   );
